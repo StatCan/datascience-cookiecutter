835b46104e9ad825ad2e0ac97f14ff94b5a14715
Andrés Solís Montero <andres.solis.montero@gmail.com>
2022-05-27 15:04:01 -0400

    Update conf.py
    Updated conf.py to point to the correct ```src``` folder.

--- {{ cookiecutter.repo_name }}/docs/conf.py
+++ {{ cookiecutter.repo_name }}/docs/conf.py
@@ -14,9 +14,10 @@
 
 import os
 import sys
-import {{ cookiecutter.package_name }}
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../src"))
+
+import {{ cookiecutter.package_name }}
 
 # -- General configuration ------------------------------------------------
 
