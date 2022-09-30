0fe8eec28dea1334bc60b3405e99c950b735185e
Reginald Maltais <reginald.maltais@gmail.com>
2022-09-29 21:51:52 -0400

    Auto-delete pypkg stubs if using R

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,29 @@
+"""
+Post project generation hook.
+
+Removes files that don't make sense based on user choices.
+"""
+
+import shutil
+from pathlib import Path
+
+
+# Files to be completely removed
+REMOVE_PATHS = [
+    '{% if cookiecutter.using_R == "Yes" %} setup.cfg {% endif %}',
+    '{% if cookiecutter.using_R == "Yes" %} pyproject.toml {% endif %}',
+    '{% if cookiecutter.using_R == "Yes" %} tests/test_api.py {% endif %}',
+    '{% if cookiecutter.using_R == "Yes" %} tests/test_cli.py {% endif %}',
+    '{% if cookiecutter.using_R == "Yes" %} src/{{ cookiecutter.repo_name.replace("-", "_") }} {% endif %}',
+]
+
+for item in REMOVE_PATHS:
+    item = item.strip()
+    if not item:
+        continue
+    item = Path(item)
+    if item.exists():
+        if item.is_dir():
+            shutil.rmtree(item)
+        else:
+            item.unlink()
