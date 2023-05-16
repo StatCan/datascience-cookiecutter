30856bf09d8e914dcd41a71935b6cccd64242d99
Reginald Maltais <reginald.maltais@statcan.gc.ca>
2023-05-16 13:12:21 -0400

    Profile testthat runner for R

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -14,6 +14,7 @@ REMOVE_PATHS = [
     '{% if cookiecutter.using_R == "Yes" %} pyproject.toml {% endif %}',
     '{% if cookiecutter.using_R == "Yes" %} tests/test_api.py {% endif %}',
     '{% if cookiecutter.using_R == "Yes" %} tests/test_cli.py {% endif %}',
+    '{% if cookiecutter.using_R == "No" %} tests/testthat {% endif %}',
     '{% if cookiecutter.using_R == "Yes" %} src/{{ cookiecutter.__pypkg }} {% endif %}',
     '{% if cookiecutter.VCS != "Gitlab" %} .gitlab-ci.yml {% endif %}',
 ]
